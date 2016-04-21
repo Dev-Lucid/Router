217429949f2248861761c368c3c2609116d53b32
Mike Thorn <mike@therapycharts.com>
2016-04-21 11:52:02 -0400

    adding unit tests

--- tests/ParseTest.php
+++ tests/ParseTest.php
@@ -8,11 +8,52 @@ class ParseTest extends \PHPUnit_Framework_TestCase
         $router = new Router();
 
         $result = $router->determineRoute('MyClass.view.MyMethod');
-        $this->assertEquals(print_r($result, true), print_r(['type'=>'view', 'class'=>'MyClass', 'method'=>'MyMethod'], true));
-        #$this->assertEqual(is_array($container->getArray()));
+        $goodResult = ['type'=>'view', 'class'=>'MyClass', 'method'=>'MyMethod'];
+        ksort($result);
+        ksort($goodResult);
+        $this->assertEquals(print_r($result, true), print_r($goodResult, true));
+
+        $result = $router->determineRoute('MyClass.controller.MyMethod');
+        $goodResult = ['type'=>'controller', 'class'=>'MyClass', 'method'=>'MyMethod'];
+        ksort($result);
+        ksort($goodResult);
+        $this->assertEquals(print_r($result, true), print_r($goodResult, true));
     }
 
     public function testAutoRoute()
     {
+        $router = new Router();
+        $router->addFixedRoute('mytestroute1', 'view',       'MyClass', 'MyMethod');
+        $router->addFixedRoute('mytestroute2', 'controller', 'MyClass', 'MyMethod');
+
+        $result = $router->determineRoute('mytestroute1');
+        $goodResult = ['type'=>'view', 'class'=>'MyClass', 'method'=>'MyMethod'];
+        ksort($result);
+        ksort($goodResult);
+        $this->assertEquals(print_r($result, true), print_r($goodResult, true));
+
+        $result = $router->determineRoute('mytestroute2');
+        $goodResult = ['type'=>'controller', 'class'=>'MyClass', 'method'=>'MyMethod'];
+        ksort($result);
+        ksort($goodResult);
+        $this->assertEquals(print_r($result, true), print_r($goodResult, true));
+    }
+
+    public function testDefaultViewProperty()
+    {
+        $router = new Router();
+
+        $result = $router->determineRoute('MyClass.view');
+        $goodResult = ['type'=>'view', 'class'=>'MyClass', 'method'=>'index'];
+        ksort($result);
+        ksort($goodResult);
+        $this->assertEquals(print_r($result, true), print_r($goodResult, true));
+
+        $router->defaultViewMethod = 'MyMethod';
+        $result = $router->determineRoute('MyClass.view');
+        $goodResult = ['type'=>'view', 'class'=>'MyClass', 'method'=>'MyMethod'];
+        ksort($result);
+        ksort($goodResult);
+        $this->assertEquals(print_r($result, true), print_r($goodResult, true));
     }
 }
--- tests/loader.php
+++
@@ -1,3 +0,0 @@
-<?php
-include(__DIR__.'/../src/php/RouterInterface.php');
-include(__DIR__.'/../src/php/Router.php');
